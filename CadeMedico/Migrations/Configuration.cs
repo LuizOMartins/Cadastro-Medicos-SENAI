73f7aa23f2c9926bd230a0e038689e51fceaaf80
LuizEduardo <luidigastech@gmail.com>
2019-04-15 10:45:46 -0300

    first commit

---
+++ CadeMedico/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace CadeMedico.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CadeMedico.DAO.EFContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CadeMedico.DAO.EFContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ CadeMedico/Models/Medico.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CadeMedico.Models {
+    public class Medico {
+        public int MedicoID { get; set; }
+        public string Nome { get; set; }
+        public string CRM { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+        public int CidadeID { get; set; }
+        public int EspecialidadeID { get; set; }
+
+
+        [ForeignKey("CidadeID")]
+        public virtual Cidade Cidade { get; set; }
+
+        [ForeignKey("EspecialidadeID")]
+        public virtual Especialidade Especialidade { get; set; }
+    }
+}
